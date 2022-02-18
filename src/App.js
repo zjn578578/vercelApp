8d9e96eddfdb0a5eaf5a722cf9d8921722d760a4
zhaojianan <835749953@qq.com>
2022-02-18 11:07:05 +0800

    init test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import img from './img.png'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={img} className="App-logo" alt="logo" />
         <p>
-          公网访问测试。
+          乌兹，永远滴神！
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello,This is Jianan
-        </a>
       </header>
     </div>
   );
